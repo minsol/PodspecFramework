cfee09fab91a702fa184b4bc34e0f3f7ac5bfae8
wanjian <wanjian@sparrowexpress.com>
2022-08-16 17:54:20 +0800

    Initial Commit

---
+++ PodspecFramework/PodspecFramework.h
@@ -0,0 +1,18 @@
+//
+//  PodspecFramework.h
+//  PodspecFramework
+//
+//  Created by wanjian on 2022/8/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodspecFramework.
+FOUNDATION_EXPORT double PodspecFrameworkVersionNumber;
+
+//! Project version string for PodspecFramework.
+FOUNDATION_EXPORT const unsigned char PodspecFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodspecFramework/PublicHeader.h>
+
+
